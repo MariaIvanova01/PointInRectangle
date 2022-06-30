ee42742186173bfab781c1b1f77c98aec1b964cb
MariaIvanova01 <mimi200801@abv.bg>
2022-06-30 14:59:15 +0300

    Abstract logic implemented.

---
+++ PointInRectangle/src/com/company/Rectangle.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topRight;
+
+    public Rectangle(Point bottomLeft, Point topRight){
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+    public boolean contains(Point newPoint){
+       return newPoint.greaterOrEqual(bottomLeft) && newPoint.lessOrEqual(topRight);
+    }
+}
